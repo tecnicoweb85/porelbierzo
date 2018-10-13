430f0e4aeecfc2e02e79b3db84b03b078146c9ce
tecnicoweb85 <36242918+tecnicoweb85@users.noreply.github.com>
2018-10-13 11:40:30 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'por-el-bierzo',
+		wp_ws_url : 'https://www.porelbierzo.com/wp-appkit-api/por-el-bierzo',
+		wp_url : 'https://www.porelbierzo.com',
+		theme : 'q-android',
+		version : '',
+		app_type : 'phonegap-build',
+		app_title : 'Por El Bierzo',
+		app_platform : 'android',
+		app_path: '',
+		gmt_offset : 0,
+		debug_mode : 'off',
+		auth_key : 'L];-p]UFE@#E4t9c6W$9!fAxJt.C;BPHl>7L,]^$rPa@nohN,<M-K*yM|2QCDDh&',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
